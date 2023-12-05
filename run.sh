506a4f4f0adeb9b1333c02fa10d4b82dfa74cb50
flover.am-powershall <211250152@smail.nju.edu.cn>
2023-12-05 10:43:14 +0800

    fix: 修改oss秘钥

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ docker stop seecooker_deploy;
 docker rm seecooker_deploy;
 docker run --name seecooker_deploy \
     -itd -p 8080:8080 \
-    -e AccesskeyID=LTAI5tN3o8739ftwMyspbAgc \
-    -e AccesskeySecret=PqJH63OBowxfXJeu5v0Tc21lX6EVvX \
+    -e OSS_ACCESS_KEY_ID=LTAI5tN3o8739ftwMyspbAgc \
+    -e OSS_ACCESS_KEY_SECRET=PqJH63OBowxfXJeu5v0Tc21lX6EVvX \
     seecooker:0.0.1
